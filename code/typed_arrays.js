676a57930c0e07468c80ff69ccde0923969a39fd
Mrunal Alatkar <alatkar.mrunal@gmail.com>
2023-04-08 14:07:19 +0000

    Typed Arrays

--- code/typed_arrays.js
+++ code/typed_arrays.js
@@ -1,1 +1,6 @@
-
+var byteSize = 64; // Needs to be multiple of 2
+var buffer = new ArrayBuffer(byteSize);
+var i32View = new Int32Array(buffer);
+console.log(buffer.byteLength); 
+console.log(i32View.byteLength); 
+console.log(i32View); 
